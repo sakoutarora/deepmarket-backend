ba6ec1c9c9cb813c9533f3b4ab24ad108503ca6c
Saksham Arora <saksham.arora@eulermotors.com>
2025-07-26 15:31:22 +0530

    adding / end point

--- routers/routes.go
+++ routers/routes.go
@@ -12,6 +12,19 @@ import (
 func Setup(app *fiber.App) {
 	app.Use(middleware.Logger())
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello from DeepMarket!",
+			"endpoints": fiber.Map{
+				"/api/v1/tickers":      "Get a list of all supported tickers",
+				"/api/v1/expiries":     "Get a list of all supported expiry dates for a given ticker",
+				"/api/v1/option_chain": "Get a list of all supported option strikes for a given ticker and expiry date",
+				"/api/v1/news":         "Get a list of all news articles for a given ticker",
+				"/ws":                  "Websocket endpoint for fetching OHLC data",
+			},
+		})
+	})
+
 	app.Get("/api/health", func(c *fiber.Ctx) error {
 		return c.SendString("Hello over HTTPS!")
 	})
